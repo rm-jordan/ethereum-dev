b0558a099979fdefb20a2f3aeab7b97b22876ce7
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-01-05 20:15:41 -0500

    created async function to connect greeting

--- src/App.js
+++ src/App.js
@@ -9,13 +9,46 @@ const greeterAddress = " 0x5fbdb2315678afecb367f032d93f642f64180aa3";
 
 function App() {
   const [greeting, setGreetingValue] = useState();
+  // if UI needed updating create another useState here instead of console.log and render it
 
-  // connects to metamask wallet of the user when we create a transaction
-  async function requestAccount() {}
+  // connects to metamask wallet of the user when we create a transaction - function prompts user
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
 
-  async function fetchGreeting() {}
+  // looking for metamask extension to be connected, if metamask is installed in browser then window.ethereum to be injected (are you using metamask? yes! --> carry on and create new provider)
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      // now create new instance of contract
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
 
-  async function setGreeting() {}
+      try {
+        const data = await contract.greet();
+        console.log("data: ", data);
+      } catch (err) {
+        console.log("Error: ", err);
+      }
+    }
+  }
+
+  async function setGreeting() {
+    if (!greeting) return;
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log({ provider });
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
 
   return (
     <div className="App">
